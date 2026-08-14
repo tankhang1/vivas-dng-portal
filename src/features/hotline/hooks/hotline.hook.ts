a98040aceb713b315ef4437afc4f58566e2e4ef6
tankhang1 <doank3442@gmail.com>
2026-08-14 16:19:59 +0700

    chore: define hook for hotline

--- src/features/hotline/hooks/hotline.hook.ts
+++ src/features/hotline/hooks/hotline.hook.ts
@@ -1,11 +1,20 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
-import { createHotlineProcess } from '@/features/hotline/api/hotline.api';
+import { createHotlineProcess, getHotline } from '@/features/hotline/api/hotline.api';
 import type { CreateHotlineProcessRequest } from '@/features/hotline/types/create-hotline-process.request';
 import type { CreateHotlineProcessResponse } from '@/features/hotline/types/create-hotline-process.response';
+import type { GetHotlineResponse } from '@/features/hotline/types/get-hotline.response';
+import { QUERY_KEY } from '@/shared/api';
 
 export function useCreateHotlineProcessMutation() {
   return useMutation<CreateHotlineProcessResponse, Error, CreateHotlineProcessRequest>({
     mutationFn: createHotlineProcess,
+  });
+}
+
+export function useHotlineQuery() {
+  return useQuery<GetHotlineResponse>({
+    queryKey: QUERY_KEY.HOTLINE,
+    queryFn: getHotline,
   });
 }
